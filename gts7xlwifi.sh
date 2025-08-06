79648edd2612ee790c138b3058f41e4df6c2369f
ravichaudhary133 <49632662+ravichaudhary133@users.noreply.github.com>
2025-08-07 01:46:51 +0530

    Update gts7xlwifi.sh

--- gts7xlwifi.sh
+++ gts7xlwifi.sh
@@ -32,7 +32,7 @@ echo "*****************************************"
 #DTB_OUT="/home/skye/bomb/out/arch/arm64/boot/dts/vendor/qcom"
 #cat $DTB_OUT/*.dtb > AnyKernel3/dtb
 
-make -j12 O=/home/skye/bomb/out $KERNEL_MAKE_ENV $BUILD_ENV dtbo.img
+make -j12 O=/home/skye/bomb/out ARCH=arm64 LLVM=1 LLVM_IAS=1 "${MAKE_OPT[@]}" dtbo.img
 DTBO_OUT="/home/skye/bomb/out/arch/arm64/boot"
 cp $DTBO_OUT/dtbo.img Anykernel/dtbo.img
 make -j12 O=/home/skye/bomb/out ARCH=arm64 LLVM=1 LLVM_IAS=1 "${MAKE_OPT[@]}" Image
