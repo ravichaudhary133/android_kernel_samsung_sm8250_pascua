56994fb6240a7e9dc79a099542b4476dd2d0474c
ravichaudhary133 <49632662+ravichaudhary133@users.noreply.github.com>
2025-08-07 01:25:06 +0530

    Update gts7xlwifi.sh

--- gts7xlwifi.sh
+++ gts7xlwifi.sh
@@ -28,13 +28,13 @@ make O=/home/skye/bomb/out ARCH=arm64 LLVM=1 LLVM_IAS=1 "${MAKE_OPT[@]}" vendor/
 echo "*****************************************"
 echo "*****************************************"
 
-make -j12 O=/home/skye/bomb/out ARCH=arm64 LLVM=1 LLVM_IAS=1 "${MAKE_OPT[@]}"  dtbs
-DTB_OUT="/home/skye/bomb/out/arch/arm64/boot/dts/vendor/qcom"
-cat $DTB_OUT/*.dtb > AnyKernel3/dtb
+#make -j12 O=/home/skye/bomb/out ARCH=arm64 LLVM=1 LLVM_IAS=1 "${MAKE_OPT[@]}"  dtbs
+#DTB_OUT="/home/skye/bomb/out/arch/arm64/boot/dts/vendor/qcom"
+#cat $DTB_OUT/*.dtb > AnyKernel3/dtb
 
-#make -j12 O=/home/skye/bomb/out $KERNEL_MAKE_ENV $BUILD_ENV dtbo.img
+make -j12 O=/home/skye/bomb/out $KERNEL_MAKE_ENV $BUILD_ENV dtbo.img
 DTBO_OUT="/home/skye/bomb/out/arch/arm64/boot"
-#cp $DTBO_OUT/dtbo.img /home/skye/bomb/dtbo.img
+cp $DTBO_OUT/dtbo.img Anykernel/dtbo.img
 make -j12 O=/home/skye/bomb/out ARCH=arm64 LLVM=1 LLVM_IAS=1 "${MAKE_OPT[@]}" Image
 IMAGE="/home/skye/bomb/out/arch/arm64/boot/Image"
 echo "**Build outputs**"
